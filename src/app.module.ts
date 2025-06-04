b4b79afbed86de0cc26b5445e4282f5379c3eb93
AboEl3iz <k.hassan202077@gmail.com>
2025-06-04 10:52:52 +0300

    files structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MoviesModule } from './theaters/movies/movies.module';
+import { MoviesModule } from './movies/movies.module';
+import { TheatersModule } from './theaters/theaters.module';
+import { ShowtimesModule } from './showtimes/showtimes.module';
+import { MoviesModule } from './reservations/movies/movies.module';
+import { AdminModule } from './admin/admin.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, MoviesModule, TheatersModule, ShowtimesModule, AdminModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
